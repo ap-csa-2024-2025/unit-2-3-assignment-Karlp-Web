e17df444009352e4e90572406ab5c318f33105e8
Karlp-Web <karlenportillo1@gamil.com>
2024-09-27 10:40:58 -0700

    yomomma

--- Classwork.java
+++ Classwork.java
@@ -11,10 +11,14 @@ public class Classwork
 
 Public static String firstLastN(String word, int n)
 {
-
-  String output = new String();
-
-  String firstN = word.substring(0,n);
+//getting the first n letter
+  String output = new String();//first n letters substring from 0 to n=2
+//getting the last n letters
+  String firstN = word.substring(0,n); //
 
   int indexOfNToLast = word.length() - n;
+
+  output = firstN + lastN;
+
+  return output;
 }
