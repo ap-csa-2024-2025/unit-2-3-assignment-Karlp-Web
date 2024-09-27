d280323b947fbbaec74db5c388330b44784c4e0f
Karlp-Web <karlenportillo1@gamil.com>
2024-09-27 10:38:05 -0700

    yomomma

--- Classwork.java
+++ Classwork.java
@@ -8,3 +8,13 @@ public class Classwork
   // write solutions to problems below
 
 }
+
+Public static String firstLastN(String word, int n)
+{
+
+  String output = new String();
+
+  String firstN = word.substring(0,n);
+
+  int indexOfNToLast = word.length() - n;
+}
